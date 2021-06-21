8f2bb729e39fc945d9d7bdac470cff26bf4aa15e
fajjarnr <zfajart@gmail.com>
2021-06-21 13:46:09 +0700

    add maps

--- src/screens/Maps/index.js
+++ src/screens/Maps/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import MapView from 'react-native-maps';
+
+const region = {
+  latitude: -6.89032304565653,
+  longitude: 109.38062960466476,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
 
 const Maps = () => {
   return (
-    <View>
-      <Text>Maps Page</Text>
+    <View style={styles.container}>
+      <MapView initialRegion={region} style={styles.container}></MapView>
     </View>
   );
 };
 
 export default Maps;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
